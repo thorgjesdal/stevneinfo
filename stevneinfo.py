895b3c9ddb0444c6aacbadd589d23407ac726fd3
Thor <thor.gjesdal@gmail.com>
2020-07-13 22:32:55 +0200

    added import of seeding marks

--- stevneinfo.py
+++ stevneinfo.py
@@ -14,12 +14,19 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.pdfgen import canvas
 from unidecode import unidecode
 from collections import defaultdict
+import requests
+from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import datetime
+import re
 #import requests
 #from bs4 import BeautifulSoup
 
 
 def read_xml_into_tree(infile):
-   with open(infile, 'rt') as f:
+   #with open(infile, 'rt') as f:
+   with open(infile, 'rb') as f:
       tree = ET.parse(f)
    return tree
 
@@ -247,9 +254,12 @@ def write_opentrack_import(tree):
     events = list_events(tree)
     events_by_athlete = sort_events_by_athlete(tree)
 
+    isodateformat = "%Y-%m-%d"
     #... write template for Results to xlsx workbook
     wb = Workbook()
     ws = wb.active
+    ws.title = 'Competitors'
+    ws1 = wb.create_sheet('Events')
     
     row_counter = 1
     ws["A1"] = 'Competitor Id'
@@ -263,7 +273,7 @@ def write_opentrack_import(tree):
     ws["I1"] = 'Event'
     ws["J1"] = 'Pb'
     ws["K1"] = 'Sb'
-    ws["M1"] = 'Event selection'
+    ws1["A1"] = 'Event selection'
     row_counter = 2
 
     jf = 0
@@ -278,21 +288,21 @@ def write_opentrack_import(tree):
             event_ref = "T%02d"%jt
 
         full_events[ ( class_code(e[0]) , e[1] ) ]  = event_ref + ' - ' + ' '.join(( class_code(e[0]), event_spec(e[1], class_code(e[0])) ))
-        ws["M%d"%row_counter] = event_ref + ' - '  + ' '.join([e[0], event_spec(e[1], class_code(e[0]))])
-        ws["O%d"%row_counter] = event_ref
-        ws["P%d"%row_counter] = event_code(e[1])
-        ws["Q%d"%row_counter] = age_group(class_code(e[0]))
-        ws["R%d"%row_counter] = gender(class_code(e[0]))
-        ws["S%d"%row_counter] = class_code(e[0])
-#       ws["S%d"%row_counter] = age_group(class_code(e[0]))
+        ws1["A%d"%row_counter] = event_ref + ' - '  + ' '.join([e[0], event_spec(e[1], class_code(e[0]))])
+        ws1["B%d"%row_counter] = event_ref
+        ws1["C%d"%row_counter] = event_code(e[1])
+        ws1["D%d"%row_counter] = age_group(class_code(e[0]))
+        ws1["E%d"%row_counter] = gender(class_code(e[0]))
+        ws1["F%d"%row_counter] = class_code(e[0])
+#       ws1["G%d"%row_counter] = age_group(class_code(e[0]))
 
-        ws["U%d"%row_counter] = ' '.join(( class_code(e[0]), event_spec(e[1], class_code(e[0])) ))
-        ws["V%d"%row_counter] = '1'
-        ws["W%d"%row_counter] = '1'
-        ws["X%d"%row_counter] = '12:00'
+        ws1["H%d"%row_counter] = ' '.join(( class_code(e[0]), event_spec(e[1], class_code(e[0])) ))
+        ws1["I%d"%row_counter] = '1'
+        ws1["J%d"%row_counter] = '1'
+        ws1["K%d"%row_counter] = '12:00'
         
         row_counter +=1
-    ws.insert_cols(13)
+#   ws.insert_cols(13)
 
     row_counter = 2    
     bib = 0
@@ -302,6 +312,7 @@ def write_opentrack_import(tree):
         fn = k[0]
         en = k[1]
         dob = '-'.join(( k[2][6:10], k[2][3:5], k[2][0:2] ))
+        dob = datetime.datetime.strptime(dob,isodateformat)
         g = k[3]
         club = k[4]
         ident = k[5]
@@ -312,9 +323,29 @@ def write_opentrack_import(tree):
             ws["C%d"%row_counter] = fn
             ws["D%d"%row_counter] = en
             ws["E%d"%row_counter] = gender(g)
-            ws["F%d"%row_counter] = dob
+            ws["F%d"%row_counter] = dob.strftime(isodateformat)
             ws["G%d"%row_counter] = club_code(club)
             ws["I%d"%row_counter] = full_events[e]
+
+            #print(e, full_events[e])
+            if not isfield(e[1]):
+                res1 = get_seed_marks(' '.join((fn, en)), dob, e[1], e[0], '2020' )
+                res2 = get_seed_marks(' '.join((fn, en)), dob, e[1], e[0], '2019' )
+                #print(res1,res2)
+                res = min(res1,res2)
+                if res=='nm':
+                    res=''
+                if e[1] == '10 000 meter':
+                    res3 = get_seed_marks(' '.join((fn, en)), dob, '5000 meter', e[0], '2020' )
+                    res4 = get_seed_marks(' '.join((fn, en)), dob, '5000 meter', e[0], '2019' )
+                    res3 = min(res3,res4)
+                    if res3 == 'nm':
+                        res3 = ''
+                    ws["L%d"%row_counter] = res3
+            else:
+                res = ''
+            ws["K%d"%row_counter] = res
+
             row_counter +=1
 
     fname = output_file_name(tree)
@@ -559,7 +590,7 @@ def event_code(event):
             u'1500 meter'        : '1500', 
             u'3000 meter'        : '3000', 
             u'5000 meter'        : '5000', 
-            u'10000 meter'       : '10000', 
+            u'10 000 meter'       : '10000', 
             u'60 meter hekk'     : '60H', 
             u'80 meter hekk'     : '80H', 
             u'100 meter hekk'    : '100H', 
@@ -1479,7 +1510,7 @@ def club_code(club_name):
        club_code=u'NEIDS'
     elif club_name in (u'Nordre Fjell Friidrett'):
        club_code=u'NFJEL'
-    elif club_name in (u'Nordre Land Idrettslag'):
+    elif club_name in (u'Nordre Land Idrettslag', u'Nordre Land IL'):
        club_code=u'NLAND'
     elif club_name in (u'Nordre Trysil IL'):
        club_code=u'NTRY'
@@ -1531,7 +1562,7 @@ def club_code(club_name):
        club_code=u'OSTU'
     elif club_name in (u'OSI Friidrett'):
        club_code=u'FRII'
-    elif club_name in (u'Oslo Politis Idrettslag'):
+    elif club_name in (u'Oslo Politis Idrettslag', u'Oslo Politis IL'):
        club_code=u'POLIT'
     elif club_name in (u'Oslostudentenes Idrettsklubb', 'Oslostudentenes IK'):
        club_code=u'OSI'
@@ -1565,7 +1596,7 @@ def club_code(club_name):
        club_code=u'RENDA'
     elif club_name in (u'Rennebu Idrettslag'):
        club_code=u'RENB'
-    elif club_name in (u'Rindal Idrettslag'):
+    elif club_name in (u'Rindal Idrettslag', u'Rindal IL'):
        club_code=u'RIND'
     elif club_name in (u'Ringerike Friidrettsklubb'):
        club_code=u'RING'
@@ -1581,13 +1612,13 @@ def club_code(club_name):
        club_code=u'ROMUL'
     elif club_name in (u'Romsdal Randoneklubb'):
        club_code=u'ROMRA'
-    elif club_name in (u'Rosendal Turnlag'):
+    elif club_name in (u'Rosendal Turnlag', u''):
        club_code=u'ROSEN'
     elif club_name in (u'Royal Sport'):
        club_code=u'ROYAL'
     elif club_name in (u'Rustad Idrettslag', u'Rustad IL'):
        club_code=u'RUS'
-    elif club_name in (u'Rygge Idrettslag'):
+    elif club_name in (u'Rygge Idrettslag', u'Rygge IL'):
        club_code=u'RYGGE'
     elif club_name in (u'Røa Allianseidrettslag'):
        club_code=u'RIL'
@@ -1645,7 +1676,7 @@ def club_code(club_name):
        club_code=u'SKAGE'
     elif club_name in (u'Skåla Idrettslag'):
        club_code=u'SKLA'
-    elif club_name in (u'Skarphedin IL'):
+    elif club_name in (u'Skarphedin IL', u'Idrettslaget Skarphedin'):
        club_code=u'SKRPH'
     elif club_name in (u'Skaubygda Il'):
        club_code=u'SKAU'
@@ -1687,7 +1718,7 @@ def club_code(club_name):
        club_code=u'SPJVK'
     elif club_name in (u'Sportsklubben Kraft'):
        club_code=u'KRAFT'
-    elif club_name in (u'Sportsklubben Rye'):
+    elif club_name in (u'Sportsklubben Rye', u'Rye Sp.Kl.'):
        club_code=u'nan'
     elif club_name in (u'Sportsklubben Rye 2'):
        club_code=u'RYE'
@@ -1749,7 +1780,7 @@ def club_code(club_name):
        club_code=u'SURN'
     elif club_name in (u'Svalbard Turn Idrettslag'):
        club_code=u'SVTU'
-    elif club_name in (u'Svarstad Idrettslag'):
+    elif club_name in (u'Svarstad Idrettslag', u'Svarstad IL'):
        club_code=u'SVARS'
     elif club_name in (u'Sveio Idrettslag'):
        club_code=u'SVEIO'
@@ -1757,7 +1788,7 @@ def club_code(club_name):
        club_code=u'SVEL'
     elif club_name in (u'Svint IL'):
        club_code=u'SVINT'
-    elif club_name in (u'SVORKMO N.O.I.'):
+    elif club_name in (u'SVORKMO N.O.I.', u'Svorkmo N.O.I.'):
        club_code=u'SVORK'
     elif club_name in (u'Sykkylven Idrottslag'):
        club_code=u'SYKK'
@@ -3113,9 +3144,100 @@ def club_name(club_code):
 
     return club_name
    
+def get_stats(event,cat,season):
+    event_id = {'100':'4', '200': 5, '400':'7', 800:'9', '1500':'11', '3000':'13', '5000':'14', '10000':'15',
+            '100H':'35', '110H':'42', '400H':'59'}
+    catcodes = {'KS': '22', 'MS': '11'}
 
- 
- 
+    print(cat, event)
+    #event = event_code(event)
+    if event in event_id.keys():
+       url = 'https://www.minfriidrettsstatistikk.info/php/LandsStatistikk.php?showclass=' + cat + '&showevent=' + event_id[event] + '&outdoor=Y&showseason=' + season + '&showclub=0'
+       r = requests.get(url)
+
+    
+       soup = BeautifulSoup(r.text, 'html.parser')
+       tables = [
+           [
+               [td.get_text(strip=True) for td in tr.find_all('td')]
+               for tr in table.find_all('tr')
+           ]
+           for table in soup.find_all('table')
+       ] 
+   
+       stats = []
+       for row in tables[0]:
+           if not row == [] and not row[0] == '-----':
+              name, club  =  row[1].split(',')
+              dob = row[2]
+              #dob = datetime.date.strptime(row[2],'%d.%m.%y')
+              perf = row[0]
+              stats.append( (name, dob, perf) )
+       #print(stats)
+       return stats
+
+def get_seed_marks(name, dob, event, cat, season): 
+    event_id = {'100':'4', '200': 5, '400':'7', 800:'9', '1500':'11', '3000':'13', '5000':'14', '10000':'15',
+            '100H':'35', '110H':'42', '400H':'59',
+            'HJ':'68', 'PV':'70', 'LJ':'71', 'TJ':'75', }
+    catcodes = {'KS': '22', 'MS': '11',
+            'G15':'6', 'G16':'7', 'G17':'8', 'G18/19':'9',
+            'J15':'17', 'J16':'18', 'J17':'19', 'J18/19':'20'}
+
+    if cat not in ('MS', 'KS'):
+        return ''
+
+    event = event_code(event)
+    cat    = catcodes[cat]
+
+    global event_stats
+    event_stats = {}
+
+    #print(event)
+    if event not in event_stats.keys():
+        event_stats[event] = {}
+    if cat not in event_stats[event]:
+        event_stats[event][cat] = {}
+    if season not in event_stats[event][cat]:
+        event_stats[event][cat][season] = get_stats(event,cat,season)
+
+    res = 'nm'
+    #print(event, cat, season)
+    s = event_stats.get(event, None)
+    if not s==None:
+        #for p in event_stats[event][cat][season]:
+        for p in s[cat][season]:
+            #print(p)
+            nme = p[0]
+            #dd = datetime.datetime.strptime(p[1], '%d.%m.%y')
+            #res = p[2]
+
+            ratio = fuzz.token_set_ratio(name, nme)
+            #print(name,nme,pratio)
+            if ratio>85:
+                res = p[2]
+                break
+
+    minsecpat = '(\d?\d)[:.,](\d\d[,.]\d?\d)'
+    match1 = re.match(minsecpat,res)
+    secwindpat = "(\d\d[,.]\d\d)[(]([+-]\d[,.]\d)[)]"
+    match2 = re.match(minsecpat,res)
+    if match1:
+        mins = match1.group(1)
+        secs = match1.group(2).replace(',','.')
+        res = mins + ':' + secs
+    elif match2:
+        res = match.group(1).replace(',','.')
+    else:
+        res = res.replace(',','.')
+    print(name,res)
+    return res
+
+
+
+
+
+
 # ...
 if len(sys.argv) < 2:
    sys.exit("Usage: %s <infile>" % sys.argv[0])
