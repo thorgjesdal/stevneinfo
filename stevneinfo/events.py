623c8f0b0a120d121363d18977d2574892552cdf
Thor Gjesdal <thor.gjesdal@gmail.com>
2023-12-27 11:15:34 +0100

    #

--- stevneinfo/events.py
+++ stevneinfo/events.py
@@ -132,6 +132,168 @@ def event_name(code):
     return event_names[code]
 
 def event_spec(event, klasse):
+    # 18.05.2020 rewrite based om implements.py form athlib
+    gender = 'F'
+    if klasse[0] in ('M', 'G'):
+        gender = 'M'
+
+    weight = ''
+    if event == 'SP' or event == 'HT':
+        if gender == 'F':
+           if klasse in ('J10', 'J11', 'J12', 'J13'):
+              weight = '2,0kg'
+           elif klasse in ('J14', 'J15', 'J15', 'J16', 'J17'):
+               weight = '3,0kg'
+           elif klasse in ('J18/19', 'KU20', 'KS', 'KV35', 'KV40', 'KV45'):
+               weight = '4,0kg'
+           elif klasse >= 'KV50':
+               weight = '3,0kg'
+        elif gender == 'M':
+           if klasse in ('G10', 'G11' ):
+              weight = '2,0kg'
+           elif klasse in ('G12', 'G13' ):
+               weight = '3,0kg'
+           elif klasse in ('G14', 'G15', 'MV70', 'MV75' ):
+               weight = '4,0kg'
+           elif klasse in ('G16', 'G17', 'MV60', 'MV65' ):
+               weight = '5,0kg'
+           elif klasse in ('G18/19', 'MU20', 'MV50', 'MV55' ):
+               weight = '6,0kg'
+           elif klasse in ('MS', 'MU23', 'MV35', 'MV40', 'MV45' ):
+               weight = '7,26kg'
+           elif klasse >= 'MV80':
+               weight = '3,0kg'
+    elif event == 'DT' :
+        if gender == 'F':
+           if klasse in ('J10', 'J11', 'J12', 'J13'):
+              weight = '0,6kg'
+           elif klasse in ('J14', 'J15'):
+               weight = '0,75kg'
+           elif klasse >= 'KV80':
+               weight = '0,75kg'
+           else:
+               weight = '1,0kg'
+        elif gender == 'M':
+           if klasse in ('G10', 'G11' ):
+              weight = '0,6kg'
+           elif klasse in ('G12', 'G13' ):
+               weight = '0,75kg'
+           elif klasse in ('G14', 'G15' ):
+               weight = '1,0kg'
+           elif klasse in ('G16', 'G17', 'MV50', 'MV55' ):
+               weight = '1,5kg'
+           elif klasse in ('G18/19', 'MU20' ):
+               weight = '1,75kg'
+           elif klasse in ('MS', 'MU23', 'MV35', 'MV40', 'MV45' ):
+               weight = '2,0kg'
+           elif klasse >= 'MV60':
+               weight = '1,0kg'
+    elif event == 'JT' :
+        if gender == 'F':
+           if klasse in ('J10', 'J11', 'J12', 'J13', 'J14'):
+              weight = '400g'
+           elif klasse in ('J15', 'J16', 'J17', 'KV50', 'KV55'):
+               weight = '500g'
+           elif klasse in ('J18/19', 'KU20', 'KU23', 'KS', 'KV35', 'KV40', 'KV45'):
+               weight = '600g'
+           elif klasse >= 'KV60':
+               weight = '400g'
+        elif gender == 'M':
+           if klasse in ('G10', 'G11', 'G13', ):
+              weight = '400g'
+           elif klasse in ('G14', 'G15', 'MV60', 'MV65' ):
+               weight = '600g'
+           elif klasse in ('G16', 'G17', 'MV50', 'MV55' ):
+               weight = '700g'
+           elif klasse in ('G18/19', 'MU20', 'MS', 'MU23', 'MV35', 'MV40', 'MV45' ):
+               weight = '800g'
+           elif klasse in ('MV70', 'MV75' ):
+               weight = '500g'
+           elif klasse >= 'MV80':
+               weight = '400g'
+    elif event == 'OT' :
+        weight='150g'
+
+
+    throws = {}
+    throws['SP'] = { 'J10' : '2,0kg', 'J11' : '2,0kg', 'J12' : '2,0kg', 'J13' : '2,0kg', 
+                       'J14' : '3,0kg', 'J15' : '3,0kg', 'J16' : '3,0kg', 'J17' : '3,0kg',
+                       'J18/19' : '4,0kg', 'KU20' : '4,0kg', 'KU23' : '4,0kg', 'KS' : '4,0kg', 
+                       'KV35' : '4,0kg', 'KV40' : '4,0kg', 'KV45' : '4,0kg', 
+                       'KV50' : '3,0kg', 'KV55' : '3,0kg', 'KV60' : '3,0kg', 'KV65' : '3,0kg', 
+                       'G10' : '2,0kg', 'G11' : '2,0kg', 'G12' : '3,0kg', 'G13' : '3,0kg', 
+                       'G14' : '4,0kg', 'G15' : '4,0kg', 'G16' : '5,0kg', 'G17' : '5,0kg',
+                       'G18/19' : '6,0kg', 'MU20' : '6,0kg', 'MU23' : '7,26kg', 'MS' : '7,26kg'
+                       } 
+    throws['DT'] = { 'J10' : '0,6kg', 'J11' : '0,6kg', 'J12' : '0,6kg', 'J13' : '0,6kg', 
+                       'J14' : '0,75kg', 'J15' : '0,75kg', 'J16' : '0,75kg', 'J17' : '0,75kg',
+                       'J18/19' : '1,0kg', 'KU20' : '1,0kg', 'KU23' : '1,0kg', 'KS' : '1,0kg', 
+                       'G10' : '0,6kg', 'G11' : '0,6kg', 'G12' : '0,75kg', 'G13' : '0,75kg', 
+                       'G14' : '1,0kg', 'G15' : '1,0kg', 'G16' : '1,5kg', 'G17' : '1,5kg',
+                       'G18/19' : '1,75kg', 'MU20' : '1,75kg', 'MU23' : '2,0kg', 'MS' : '2,0kg', 
+                       'MV35' : '2,0kg', 'MV40' : '2,0kg', 'MV45' : '2,0kg',
+                       'MV50' : '1,5kg', 'MV55' : '1,5kg', 
+                       'MV60' : '1,0kg', 'MV65' : '1,0kg', 'MV70' : '1,0kg', 'MV75' : '1,0kg' 
+                       } 
+    throws['HT'] = { 'J10' : '2,0kg', 'J11' : '2,0kg', 'J12' : '2,0kg', 'J13' : '2,0kg', 
+                       'J14' : '3,0kg', 'J15' : '3,0kg', 'J16' : '3,0kg', 'J17' : '3,0kg',
+                       'J18/19' : '4,0kg', 'KU20' : '4,0kg', 'KU23' : '4,0kg', 'KS' : '4,0kg', 
+                       'G10' : '2,0kg', 'G11' : '2,0kg', 'G12' : '3,0kg', 'G13' : '3,0kg', 
+                       'G14' : '4,0kg', 'G15' : '4,0kg', 'G16' : '5,0kg', 'G17' : '5,0kg',
+                       'G18/19' : '6,0kg', 'MU20' : '6,0kg', 'MU23' : '7,26kg', 'MS' : '7,26kg'} 
+    throws['JT'] = { 'J10' : '400g', 'J11' : '400g', 'J12' : '400g', 'J13' : '400g', 
+                       'J14' : '400g', 'J15' : '500g', 'J16' : '500g', 'J17' : '500g',
+                       'J18/19' : '600g', 'KU20' : '600g', 'KU23' : '600g', 'KS' : '600g', 
+                       'G10' : '400g', 'G11' : '400g', 'G12' : '400g', 'G13' : '400g', 
+                       'G14' : '600g', 'G15' : '600g', 'G16' : '700g', 'G17' : '700g',
+                       'G18/19' : '800g', 'MU20' : '800g', 'MU23' : '800g', 'MS' : '800g'} 
+    throws['OT'] = { 'J10' : '150g', 'J11' : '150g', 'J12' : '150g', 'J13' : '150g', 'J14' : '150g', 
+                             'G10' : '150g', 'G11' : '150g', 'G12' : '150g', 'G13' : '150g', 'G14' : '150g' }
+    hurdles = {}
+    hurdles['60H'] = { 'J10' : '68,0cm', 'J11' : '68,0cm', 'J12' : '76,2cm', 'J13' : '76,2cm', 'J14' : '76,2cm',
+                                 'J15' : '76,2cm', 'J16' : '76,2cm', 'J17' : '76,2cm',
+                                 'J18/19' : '84,0cm','KU20' : '84,0cm', 'KU23' : '84,0cm', 'KS' : '84,0cm',
+                                 'KV50' : '76,2cm',
+                                 'G10' : '68,0cm', 'G11' : '68,0cm', 'G12' : '76,2cm', 'G13' : '76,2cm', 'G14' : '84,0cm',
+                                 'G15' : '84,0cm', 'G16' : '91,4cm', 'G17' : '91,4cm',
+                                 'G18/19' : '100cm','MU20' : '100cm', 'MU23' : '106,7cm', 'MS' : '106,7cm' ,
+                                 'MV35':'100cm', 'MV40':'91,4cm', 'MV45':'91,4cm', 'MV50':'91,4cm', 'MV55':'91,4cm', 
+                                 'MV60':'84cm', 'MV65':'84cm', 'MV70':'76,2cm', 'MV75':'76,2cm', 
+                                 }
+    hurdles['80H'] = { 'J15' : '76,2cm', 'J16' : '76,2cm', 'G14' : '84,0cm' } 
+    hurdles['100H'] = { 'J16' : '76,2cm', 'J17' : '76,2cm', 'J18/19' : '84,0cm','KU20' : '84,0cm', 'KU23' : '84,0cm', 'KS' : '84,0cm',
+                                 'G15' : '84,0cm', 'G16' : '91,4cm'}
+    hurdles['110H'] = { 'G17' : '91,4cm', 'G18/19' : '100cm','MU20' : '100cm', 'MU23' : '106,7cm', 'MS' : '106,7cm' }
+    hurdles['200H'] = { 'J10' : '68,0cm', 'J11' : '68,0cm', 'J12' : '68,0cm', 'J13' : '68,0cm', 'J14' : '76,2cm',
+                                 'J15' : '76,2cm', 'J16' : '76,2cm', 'J17' : '76,2cm',
+                                 'J18/19' : '76,2cm','KU20' : '76,2cm', 'KU23' : '76,2cm', 'KS' : '76,2cm',
+                                 'G10' : '68,0cm', 'G11' : '68,0cm', 'G12' : '68,0cm', 'G13' : '68,0cm', 'G14' : '76,2cm',
+                                 'G15' : '76,2cm', 'G16' : '76,2cm', 'G17' : '76,2cm',
+                                 'G18/19' : '76,2cm','MU20' : '76,2cm', 'MU23' : '76,2cm', 'MS' : '76,2cm' }
+    hurdles['300H'] = { 'J15' : '76,2cm', 'J16' : '76,2cm', 'J17' : '76,2cm',
+                                 'J18/19' : '76,2cm','KU20' : '76,2cm', 'KU23' : '76,2cm', 'KS' : '76,2cm',
+                                 'G15' : '76,2cm', 'G16' : '84,0cm', 'G17' : '84,0cm',
+                                 'G18/19' : '91,4cm','MU20' : '91,4cm', 'MU23' : '91,4cm', 'MS' : '91,4cm',
+                                 'default':''}
+    hurdles['400H'] = { 'J15' : '76,2cm', 'J16' : '76,2cm', 'J17' : '76,2cm',
+                                 'J18/19' : '76,2cm','KU20' : '76,2cm', 'KU23' : '76,2cm', 'KS' : '76,2cm',
+                                 'G15' : '76,2cm', 'G16' : '84,0cm', 'G17' : '84,0cm',
+                                 'G18/19' : '91,4cm','MU20' : '91,4cm', 'MU23' : '91,4cm', 'MS' : '91,4cm' }
+
+
+    if event in ('SP', 'DT', 'JT', 'HT', 'OT'):
+       #e = event + ' ' + throws[event][klasse]
+#       e = event_name(event) + ' ' + throws[event].get(klasse,'')
+       e = event_name(event) + ' ' + weight
+    elif event in ('60H', '80H', '100H', '110H', '200H', '300H', '400H'): 
+#      e = event_name(event) + ' ' + hurdles[event][klasse]
+       e = event_name(event) + ' ' + hurdles[event].get(klasse,'')
+    else:
+       e = event_name(event)
+
+    return e
+"""
+def event_spec(event, klasse):
     throws = {}
     throws['Kule'] = { 'J10' : '2,0kg', 'J11' : '2,0kg', 'J12' : '2,0kg', 'J13' : '2,0kg', 
                        'J14' : '3,0kg', 'J15' : '3,0kg', 'J16' : '3,0kg', 'J17' : '3,0kg',
@@ -206,7 +368,7 @@ def event_spec(event, klasse):
        e = event_name(event)
 
     return e
-
+"""
 
 
 def sort_event_list(events):
