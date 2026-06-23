1f36b8ee68b49385df21b5d83b285a5c38bb089c
Thor Gjesdal <thor.gjesdal@gmail.com>
2026-06-23 14:48:52 +0200

    #

--- event_crosstable.py
+++ event_crosstable.py
@@ -1,6 +1,13 @@
+"""
+TODO:
+      * sort per category
+      * finish conflics list
+      *
+"""
 import json
 import pandas as pd
 from collections import defaultdict
+import pprint
 
 # Load your JSON file
 with open("nordicmasters.json") as f:
@@ -16,6 +23,7 @@ gender_map = {
     "F": defaultdict(set)
 }
 
+
 for e in entries:
     athlete = e["competitorId"]
     gender = e["gender"]
@@ -24,6 +32,13 @@ for e in entries:
 
         if gender in gender_map:
             gender_map[gender][athlete].add(event)
+
+athletes = {}
+for e in entries:
+    athlete = e["competitorId"]
+    name = e['fullName']
+    athletes[athlete] = name
+#print(athletes)
 
 
 def sort_events(e):
@@ -38,7 +53,7 @@ def build_matrix(athlete_events):
 
 
     #events.sort(key=sort_events)
-    print(events)
+    #print(events)
 
     # empty matrix
     df = pd.DataFrame(0, index=events, columns=events)
@@ -53,6 +68,47 @@ def build_matrix(athlete_events):
             df.loc[ev1, ev2] = count
 
     return df
+
+def build_and_save_conflicts_table(athlete_events):
+    events = sorted({ev for evs in athlete_events.values() for ev in evs}, key=sort_events)
+    #print ( athlete_events )
+
+    conflicts = {}
+    for ev1 in events:
+        for ev2 in events:
+            if ev1==ev2:
+                continue
+            if ev1 not in conflicts:
+                conflicts[ev1] = {}
+            for bib in athlete_events.keys():
+                evs = athlete_events[bib]
+                if ev1 in evs and ev2 in evs:
+                    if ev2 not in conflicts[ev1]:
+                        conflicts[ev1][ev2]=[]
+                    conflicts[ev1][ev2].append(athletes[bib])
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(conflicts)
+     
+
+
+    """
+    # empty matrix
+    df = pd.DataFrame( None, index=events, columns=events, dtype=object )
+
+    print( 'test', df.loc['100','100']  )
+
+    # fill
+    for ev1 in events:
+        for ev2 in events:
+            count = sum(
+                1 for evs in athlete_events.values()
+                if ev1 in evs and ev2 in evs
+            )
+            df.loc[ev1, ev2] = count
+"""
+
+test = build_and_save_conflicts_table(gender_map["F"])
 
 male_matrix = build_matrix(gender_map["M"])
 female_matrix = build_matrix(gender_map["F"])
